f87d6e0bb10833f13846c4a031060f6340ea3ca0
speedracr <jonas@robocomobo.com>
2013-11-25 17:14:09 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bootstrapping::Application.config.secret_key_base = 'fcd9b601e50309d296db00a7ec3ebc1c9460f248b017426636ec456a7d6e714090e765fa57ff1ae5be13a542ded78a43922816c20aa4258f1c5f8f59d9a65f94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bootstrapping::Application.config.session_store :cookie_store, key: '_bootstrapping_session'
